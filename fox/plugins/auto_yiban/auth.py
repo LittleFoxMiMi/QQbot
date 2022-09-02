a88a5d531b2979b45cc6e1511d2acacae8ea68c1
LittleFoxTail <1216878448@qq.com>
2022-09-02 15:14:41 +0800

    fix yiban

--- fox/plugins/auto_yiban/auth.py
+++ fox/plugins/auto_yiban/auth.py
@@ -2,8 +2,11 @@ from littlepaimon_utils import aiorequests
 import asyncio
 import json
 import base64
+import aiofiles
+import ujson
 import time
 import httpx
+#from yiban import Yiban as YB
 from .ddddocr import DdddOcr
 
 ocr = DdddOcr()
@@ -18,6 +21,7 @@ current_design = ""
 morning_content = "24%5B0%5D%5B0%5D%5Bname%5D=form%5B24%5D%5Bfield_1588749561_2922%5D%5B%5D&24%5B0%5D%5B0%5D%5Bvalue%5D=36.5&24%5B0%5D%5B1%5D%5Bname%5D=form%5B24%5D%5Bfield_1588749738_1026%5D%5B%5D&24%5B0%5D%5B1%5D%5Bvalue%5D=%E6%B5%99%E6%B1%9F%E7%9C%81+%E5%98%89%E5%85%B4%E5%B8%82+%E5%B9%B3%E6%B9%96%E5%B8%82+%E6%9D%BE%E6%9E%AB%E6%B8%AF%E8%B7%AF+616%E5%8F%B7+%E9%9D%A0%E8%BF%91%E4%B9%9D%E9%BE%99%E8%8A%B1%E8%8B%91+&24%5B0%5D%5B2%5D%5Bname%5D=form%5B24%5D%5Bfield_1588749759_6865%5D%5B%5D&24%5B0%5D%5B2%5D%5Bvalue%5D=%E6%98%AF&24%5B0%5D%5B3%5D%5Bname%5D=form%5B24%5D%5Bfield_1588749842_2715%5D%5B%5D&24%5B0%5D%5B3%5D%5Bvalue%5D=%E5%90%A6&24%5B0%5D%5B4%5D%5Bname%5D=form%5B24%5D%5Bfield_1588749886_2103%5D%5B%5D&24%5B0%5D%5B4%5D%5Bvalue%5D="
 noon_content = "25%5B0%5D%5B0%5D%5Bname%5D=form%5B25%5D%5Bfield_1588750276_2934%5D%5B%5D&25%5B0%5D%5B0%5D%5Bvalue%5D=36.6&25%5B0%5D%5B1%5D%5Bname%5D=form%5B25%5D%5Bfield_1588750304_5363%5D%5B%5D&25%5B0%5D%5B1%5D%5Bvalue%5D=%E6%B5%99%E6%B1%9F%E7%9C%81+%E5%98%89%E5%85%B4%E5%B8%82+%E5%B9%B3%E6%B9%96%E5%B8%82+%E6%98%A5%E6%99%96%E8%B7%AF+500%E5%8F%B7+%E9%9D%A0%E8%BF%91%E5%B9%B3%E6%B9%96%E5%B8%82%E7%AC%AC%E4%B8%80%E4%BA%BA%E6%B0%91%E5%8C%BB%E9%99%A2+&25%5B0%5D%5B2%5D%5Bname%5D=form%5B25%5D%5Bfield_1588750323_2500%5D%5B%5D&25%5B0%5D%5B2%5D%5Bvalue%5D=%E6%98%AF&25%5B0%5D%5B3%5D%5Bname%5D=form%5B25%5D%5Bfield_1588750343_3510%5D%5B%5D&25%5B0%5D%5B3%5D%5Bvalue%5D=%E5%90%A6&25%5B0%5D%5B4%5D%5Bname%5D=form%5B25%5D%5Bfield_1588750363_5268%5D%5B%5D&25%5B0%5D%5B4%5D%5Bvalue%5D="
 current_content = ""
+yiban_token = ""
 
 
 async def time_set():
@@ -36,16 +40,15 @@ async def time_set():
 
 
 async def auth():
-    headers = {
+    headers1 = {
         "Host": "f.yiban.cn",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; LM-F100 Build/QKQ1.200719.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.69 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "authorization": "Bearer 7ddaad13f1d6c5ebd404c91b8d46be8a",
+        "authorization": f"Bearer {yiban_token}",
         "appversion": "5.0.12",
-        "logintoken": "7ddaad13f1d6c5ebd404c91b8d46be8a",
-        "signature": "UBfR234+qAy70MTcciL12lOYTvDDRZZbIhPXn9dU2THMiFDkOISmWaYEVi5DNXeK/v2F9TUvlzzGEtI5OKwQSw",
+        "logintoken": yiban_token,
         "X-Requested-With": "com.yiban.app",
         "Sec-Fetch-Site": "none",
         "Sec-Fetch-Mode": "navigate",
@@ -53,14 +56,33 @@ async def auth():
         "Sec-Fetch-Dest": "document",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cookie": "loginToken=7ddaad13f1d6c5ebd404c91b8d46be8a; client=android; https_waf_cookie=ed07fcc4-a4f0-495daf19a294173d8b6a5435c0753fc89302; _YB_OPEN_V2_0=2lf0210P27PLou0h; yibanM_user_token=7ddaad13f1d6c5ebd404c91b8d46be8a",
+        "Cookie": f"loginToken={yiban_token}; client=android",
     }
-    req1 = await aiorequests.get("https://f.yiban.cn/iapp610661", headers=headers)
+    req1 = await aiorequests.get("https://f.yiban.cn/iapp610661", headers=headers1)
     print("易班登录认证1："+str(req1.status_code))
     if req1.status_code != 302:
         return "1"
+    cookie1 = str(req1.cookies).split(" ")[1]
+    headers2 = {
+        "Host": "f.yiban.cn",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; LM-F100 Build/QKQ1.200719.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.69 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "authorization": f"Bearer {yiban_token}",
+        "appversion": "5.0.12",
+        "logintoken": yiban_token,
+        "X-Requested-With": "com.yiban.app",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cookie": f"loginToken={yiban_token}; client=android; {cookie1}",
+    }
     req2 = await aiorequests.get(
-        "https://f.yiban.cn/iapp/index?act=iapp610661", headers=headers)
+        "https://f.yiban.cn/iapp/index?act=iapp610661", headers=headers2)
     print("易班登录认证2："+str(req2.status_code))
     if req2.status_code != 302:
         return "1"
@@ -77,7 +99,7 @@ async def sust_auth1(location):
         "X-Requested-With": "com.yiban.app",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cookie": "PHPSESSID=9a42777bd25bbc36364eb0ef9cc90171",
+        "Cookie": f"PHPSESSID={yiban_token}",
     }
     req = await aiorequests.get(location, headers=headers, timeout=30)
     print("sust登录认证1："+str(req.status_code))
@@ -96,7 +118,7 @@ async def sust_auth2(location):
         "X-Requested-With": "com.yiban.app",
         "Accept-Encoding": "gzip, deflate",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Cookie": "PHPSESSID=9a42777bd25bbc36364eb0ef9cc90171",
+        "Cookie": f"PHPSESSID={yiban_token}",
     }
     location = "http://yiban.sust.edu.cn/v4"+location[2:]
     req = await aiorequests.get(location, headers=headers, timeout=30)
@@ -116,7 +138,7 @@ async def get_img():
         'Connection': 'Keep-Alive',
         'Accept-Encoding': 'gzip,deflate',
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; LM-F100 Build/QKQ1.200719.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.69 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12",
-        "Cookie": "PHPSESSID=9a42777bd25bbc36364eb0ef9cc90171",
+        "Cookie": f"PHPSESSID={yiban_token}",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     }
     req = await aiorequests.post(
@@ -145,7 +167,7 @@ async def post_form(captcha_info):
         'Connection': 'Keep-Alive',
         'Accept-Encoding': 'gzip,deflate',
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; LM-F100 Build/QKQ1.200719.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.69 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12",
-        "Cookie": "PHPSESSID=9a42777bd25bbc36364eb0ef9cc90171",
+        "Cookie": f"PHPSESSID={yiban_token}",
         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
     }
     req = await aiorequests.post(f"http://yiban.sust.edu.cn/v4/public/index.php/Index/formtime/add.html?desgin_id={current_design}&list_id=12&verify={captcha_info['captcha_code']}&uniqid={captcha_info['captcha_id']}", headers=headers, data=current_content, timeout=30)
@@ -185,10 +207,22 @@ async def get_captcha():
 
 
 async def yiban():
+    global yiban_token
+    user = await data_load(path+"user_info.json")
+    if not user:
+        return ("文件读取错误")
+    yiban_token = user["user_token"]
     await time_set()
     location = await auth()
     if location == "1":
-        return ("易班验证失败！")
+        # print("易班验证失败！尝试重新登录")
+        #yb = YB(user["phone_number"], user["password"])
+        #yiban_token = yb.get_user_access_token()
+        # location = await auth()
+        if location == "1":
+            return ("易班验证失败！")
+        #user["user_token"] = yiban_token
+        # await write_down(path+"user_info.json", user)
     for i in range(5):
         try:
             print(f"sust验证1（{i}/5）")
@@ -232,6 +266,25 @@ async def yiban():
                 continue
             return e
 
+
+async def data_load(json_ad):
+    try:
+        async with aiofiles.open(json_ad, "r", encoding="utf-8")as f:
+            text = await f.read()
+            data = ujson.loads(text)
+        return data
+    except:
+        print("文件读取错误！")
+
+
+async def write_down(json_ad, data):
+    try:
+        async with aiofiles.open(json_ad, "w") as f:
+            text = ujson.dumps(data)
+            await f.write(text)
+        print("token已更新")
+    except Exception as e:
+        print("文件写入错误！")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
