5efe46451f2c848c0e924606fe092dd3014d4c89
LittleFoxTail <1216878448@qq.com>
2022-11-14 11:07:28 +0800

    fix ddcheck

--- fox/plugins/ddcheck/config.py
+++ fox/plugins/ddcheck/config.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Extra
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    bilibili_cookie: str = "SESSDATA=b9f6be9a%2C1676169660%2C4a02c%2A81;"
+    bilibili_cookie: str = "SESSDATA=4dc4e6c6%2C1683947081%2Ca1610%2Ab2;"
